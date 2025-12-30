8a628cc3d896bb4219af6dfe952eb5cac7853232
Denis Gaev <denisgaev@live.ru>
2025-12-30 20:39:12 +0500

    Introduce AdminLayout component with AuthBarrier integration for child components

---
+++ services/frontend/src/app/admin/layout.tsx
@@ -0,0 +1,5 @@
+import { AuthBarrier } from '@/components/auth-check';
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return <AuthBarrier>{children}</AuthBarrier>;
+}
