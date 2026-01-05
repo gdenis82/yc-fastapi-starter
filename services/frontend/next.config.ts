9581eed72b73576073211b73cda0ca49c92aeda9
Denis Gaev <denisgaev@live.ru>
2026-01-05 14:29:32 +0500

    Refine `docker-compose.yml` and `next.config.ts`: adjust npm dev command for webpack usage, add conditional webpack config based on environment.

--- services/frontend/next.config.ts
+++ services/frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  /* config options here */
+  output: process.env.NODE_ENV === 'production' ? 'standalone' : undefined,
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
