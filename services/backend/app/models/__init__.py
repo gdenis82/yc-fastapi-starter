fe347b13ecbc11462bcbc4cd0366e4b16da7ce84
Denis Gaev <denisgaev@live.ru>
2026-01-05 18:26:04 +0500

    Add `TokenDenylist` model to `__init__.py` exports

--- services/backend/app/models/__init__.py
+++ services/backend/app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.base import Base
 from app.models.user import User
 from app.models.role import Role
+from app.models.token_denylist import TokenDenylist
 
-__all__ = ["Base", "User", "Role"]
+__all__ = ["Base", "User", "Role", "TokenDenylist"]
