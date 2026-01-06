8993df9129c3df31cee35246c27dbafd4bfa523e
Denis Gaev <denisgaev@live.ru>
2026-01-06 16:06:45 +0500

    Remove `TokenDenylist` import and entry from `__all__` in models init file

--- services/backend/app/models/__init__.py
+++ services/backend/app/models/__init__.py
@@ -1,6 +1,5 @@
 from app.models.base import Base
 from app.models.user import User
 from app.models.role import Role
-from app.models.token_denylist import TokenDenylist
 
-__all__ = ["Base", "User", "Role", "TokenDenylist"]
+__all__ = ["Base", "User", "Role"]
