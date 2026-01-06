7e8f6c15b641e99b9409c614a6b8da172413b294
Denis Gaev <denisgaev@live.ru>
2026-01-06 22:39:19 +0500

    Remove unnecessary tag parameters in API router definitions

--- services/backend/app/api/api.py
+++ services/backend/app/api/api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.api.endpoints import root, auth, admin, password
 
 api_router = APIRouter()
-api_router.include_router(root.router, tags=["root"])
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
-api_router.include_router(password.router, prefix="/auth", tags=["auth"])
+api_router.include_router(root.router)
+api_router.include_router(auth.router, prefix="/auth")
+api_router.include_router(admin.router, prefix="/admin")
+api_router.include_router(password.router, prefix="/auth")
